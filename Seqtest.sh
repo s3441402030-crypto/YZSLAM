181b4f4934be71314aa6d88c1f771cb77fb35b0e
YZ111 <s3441402030@gmail.com>
2026-06-28 16:18:21 +0800

    Update Seqtest.sh

---
+++ Seqtest.sh
@@ -0,0 +1,2 @@
+./Examples/RGB-D/rgbd_tum Vocabulary/ORBvoc.txt Examples/RGB-D/TUM3.yaml datasets/TUM/rgbd_dataset_freiburg3_walking_xyz datasets/TUM/rgbd_dataset_freiburg3_walking_xyz/associations.txt
+./Examples/RGB-D/rgbd_tum Vocabulary/ORBvoc.txt Examples/RGB-D/Bonn.yaml datasets/Bonn/rgbd_bonn_crowd datasets/Bonn/rgbd_bonn_crowd/associations.txt
